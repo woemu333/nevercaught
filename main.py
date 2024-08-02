5289e310c81ddd93503ef732ad075c04836894c5
domino6658 <123335575+domno6658@users.noreply.github.com>
2024-08-02 21:40:03 +1000

    fix

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ import subprocess
 import os
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-subprocess.Popen('python cbot1.py')
-subprocess.Popen('python cbot2.py')
+subprocess.Popen('cbot1.py')
+subprocess.Popen('cbot2.py')
