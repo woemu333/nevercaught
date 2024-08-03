f2d3e24776b86c0de11371f2d60d1b2a5011e43d
domino6658 <123335575+domno6658@users.noreply.github.com>
2024-08-03 11:02:00 +1000

    udpoate

---
+++ cbot3.py
@@ -0,0 +1,34 @@
+#!~/.venvs/3.12/bin/python3.12
+import selfcord
+from selfcord.ext import tasks
+import os
+import yaml
+import random
+import asyncio
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+def getconfig():
+    with open('config.yml', 'r') as file:
+        config = yaml.safe_load(file)
+    return config
+config = getconfig()
+
+client = selfcord.Client()
+
+@client.event
+async def on_ready():
+    await asyncio.sleep(20)
+    task1.start()
+    print('ready2')
+
+
+@tasks.loop(minutes=0.5)
+async def task1():
+    for serverid in config['server_ids_2']:
+        guild = client.get_guild(serverid)
+        channel = guild.text_channels[0]
+        await channel.send(random.randint(1,1000))
+        await asyncio.sleep(8)
+
+client.run(config['token3'])
